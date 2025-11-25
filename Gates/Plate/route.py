a9c73632cf1feab77871c10e01685f046d5dd1e8
jchanho99 <jchanho99@gmail.com>
2025-11-25 20:50:39 +0900

    띄어쓰기 수정

--- Gates/Plate/route.py
+++ Gates/Plate/route.py
@@ -8,16 +8,18 @@ app = FastAPI()
 def is_healthy():
     return {"status": "Service is running"}
 
-# 비동기 함수(async def)로 GET 요청 처리
+
+# 공장 문 개방 비동기 처리
 @app.get("/door/open")
 async def open():
     
     # 실제 DB 조회나 외부 API 호출과 같은 I/O 작업 시뮬레이션
     # asyncio.sleep은 비동기적으로 작동하여, 이 대기 시간 동안 서버는 다른 요청을 처리할 수 있습니다.
     await asyncio.sleep(1)
-    
     return {"message": f"Opening the door..."}
 
+
+# 공장 문 닫힘 비동기 처리
 @app.get("/door/close")
 async def close():
     await asyncio.sleep(1)
