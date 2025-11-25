e884c85a111c808b7aed388ff1e011a577cda062
jchanho99 <jchanho99@gmail.com>
2025-11-25 20:30:36 +0900

    close API 추가

--- Gates/Plate/route.py
+++ Gates/Plate/route.py
@@ -4,13 +4,13 @@ import asyncio
 app = FastAPI()
 
 # 서버 정상작동 여부를 판단하는 동기 요청 처리
-@app.get("/door/health")
+@app.get("/door/healthy")
 def is_healthy():
     return {"status": "Service is running"}
 
 # 비동기 함수(async def)로 GET 요청 처리
 @app.get("/door/open")
-async def open_sesame():
+async def open():
     
     # 실제 DB 조회나 외부 API 호출과 같은 I/O 작업 시뮬레이션
     # asyncio.sleep은 비동기적으로 작동하여, 이 대기 시간 동안 서버는 다른 요청을 처리할 수 있습니다.
@@ -18,3 +18,7 @@ async def open_sesame():
     
     return {"message": f"Opening the door..."}
 
+@app.get("/door/close")
+async def close():
+    await asyncio.sleep(1)
+    return {"message": f"Closing the door..."}
